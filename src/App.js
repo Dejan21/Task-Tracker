76aee41e92d1e36a45829ab26b23b5dd54bef35e
Dejan21 <jangelovski_dejan@yahoo.com>
2022-05-12 15:01:53 +0200

    Task comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//import React from "react";  need for class compononet but not for Func
+import { useState } from "react"
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id:1, 
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+           id:2, 
+           text: 'Meeting at School',
+           day: 'Feb 6th at 1:30pm',
+           reminder: true,
+        },
+        {
+           id:3, 
+           text: 'Food Shopping',
+           day: 'Feb 5th at 2:30pm',
+           reminder: false,
+        }
+    ]
+)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Task Tracker'/>
+      <Tasks tasks={tasks}/>
     </div>
-  );
+  )
 }
+
+// class comp
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hell ofrom a class </h1>
+//   }
+// }
 
 export default App;
